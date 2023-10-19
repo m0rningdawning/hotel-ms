90e6b92a5684be7e6a7c4e108657f560f456b9c1
Pavlo Korolov <bibonga54@gmail.com>
2023-10-19 21:42:12 +0200

    Database connection test

--- app/back-end/hotelms/src/main/java/com/pavlokaganise/hotelms/HotelmsApplication.java
+++ app/back-end/hotelms/src/main/java/com/pavlokaganise/hotelms/HotelmsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HotelmsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HotelmsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HotelmsApplication.class, args);
+    }
 
 }
