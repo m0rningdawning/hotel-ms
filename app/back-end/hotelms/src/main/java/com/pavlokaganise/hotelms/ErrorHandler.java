625e50b2cc9616bb11b7f2055017b1f5af1637eb
Pavlo Korolov <bibonga54@gmail.com>
2023-10-20 13:03:49 +0200

    Pseudo error controller

---
+++ app/back-end/hotelms/src/main/java/com/pavlokaganise/hotelms/ErrorHandler.java
@@ -0,0 +1,20 @@
+package com.pavlokaganise.hotelms;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ErrorHandler implements ErrorController {
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request) {
+        int statusCode = (int) request.getAttribute("javax.servlet.error.status_code");
+
+        return switch (statusCode) {
+            case 404 -> "404 - Not found!";
+            case 500 -> "500 - Internal Server Error";
+            default -> "An error occurred";
+        };
+    }
+}
--- app/back-end/hotelms/src/main/java/com/pavlokaganise/hotelms/HotelmsApplication.java
+++ app/back-end/hotelms/src/main/java/com/pavlokaganise/hotelms/HotelmsApplication.java
@@ -1,13 +1,25 @@
 package com.pavlokaganise.hotelms;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@ComponentScan(basePackages = "com.pavlokaganise.hotelms")
 public class HotelmsApplication {
+    @Autowired
+    public ErrorHandler errorHandler;
 
     public static void main(String[] args) {
         SpringApplication.run(HotelmsApplication.class, args);
     }
 
+    @GetMapping("/test")
+    public String endpointRoot() {
+        return "This is the root end point!";
+    }
 }
