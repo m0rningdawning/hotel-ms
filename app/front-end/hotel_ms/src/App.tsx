27c0e2293238d5445ad4766ba1c0467500c0667e
Pavlo Korolov <bibonga54@gmail.com>
2023-10-27 22:29:45 +0200

    Cleanup and bootstrap added

--- app/front-end/hotel_ms/src/App.tsx
+++ app/front-end/hotel_ms/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import AppRouter from './AppRouter';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <AppRouter />
--- app/front-end/hotel_ms/src/components/Home/Home.tsx
+++ app/front-end/hotel_ms/src/components/Home/Home.tsx
@@ -7,8 +7,13 @@ const Home = () => {
     height: "150px",
   };
 
+  const linkList = [
+    { to: "*", text: "Test 404 page" },
+    { to: "/admin", text: "Go to Admin Page" },
+  ];
+
   return (
-    <div>
+    <>
       <h1>Home Page</h1>
       <div>
         <img
@@ -18,8 +23,15 @@ const Home = () => {
           alt="Raccoon"
         />
       </div>
-      <Link to="/admin">Go to Admin Page</Link>
-    </div>
+      <ul className="list-group">
+        <li className="list-group-item">
+          <Link to={linkList[1].to}>{linkList[1].text}</Link>
+        </li>
+        <li className="list-group-item">
+          <Link to={linkList[0].to}>{linkList[0].text}</Link>
+        </li>
+      </ul>
+    </>
   );
 };
 
