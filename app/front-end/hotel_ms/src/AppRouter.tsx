66eb1a42700f714f85f906b3bcc3a98f82f905d9
Pavlo Korolov <bibonga54@gmail.com>
2023-10-29 14:54:05 +0100

    Structure improvement

--- app/front-end/hotel_ms/src/AppRouter.tsx
+++ app/front-end/hotel_ms/src/AppRouter.tsx
@@ -5,9 +5,9 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
-import Home from "./components/Home/Home";
-import Admin from "./components/Admin/Admin";
-import NotFound from "./components/NotFound/NotFound";
+import Home from "./pages/Home/Home";
+import Admin from "./pages/Admin/Admin";
+import NotFound from "./pages/NotFound/NotFound";
 
 const AppRouter = () => {
   return (
