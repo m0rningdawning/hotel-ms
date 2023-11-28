d98826794fdb9d8e2a1316dcbaae202d34a0b02d
Pavlo Korolov <bibonga54@gmail.com>
2023-11-28 23:59:03 +0100

    Admin page improvements

---
+++ app/front-end/hotel_ms/src/components/home-carousel/HomeCarousel.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import Carousel from "react-bootstrap/Carousel";
+import "./HomeCarousel.css";
+
+const HomeCarousel = () => {
+  return (
+    <center>
+      <Carousel className="carousel-home">
+        <Carousel.Item>
+          <img
+            src="https://imageio.forbes.com/specials-images/imageserve/5cdb058a5218470008b0b00f/Nobu-Ryokan-Malibu/0x0.jpg?format=jpg&height=1009&width=2000"
+            className="d-block carousel-img"
+            alt="Room 1"
+          ></img>
+          <Carousel.Caption>
+            <h3>Luxury Room 1</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            src="https://hotelandra.com/wp-content/uploads/2022/01/Andra2483-Andra-Queen-Queen.jpg"
+            className="d-block carousel-img"
+            alt="Room 2"
+          ></img>
+          <Carousel.Caption>
+            <h3>Luxury Room 2</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            src="https://www.redrockresort.com/wp-content/uploads/2020/12/RR-Standard-2-Queen.jpg"
+            className="d-block carousel-img"
+            alt="Room 3"
+          ></img>
+          <Carousel.Caption>
+            <h3>Luxury Room 3</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </center>
+  );
+};
+
+export default HomeCarousel;
--- app/front-end/hotel_ms/src/pages/Admin/Admin.tsx
+++ app/front-end/hotel_ms/src/pages/Admin/Admin.tsx
@@ -1,18 +1,45 @@
 import React from "react";
 import HmsFooter from "../../components/footer/HmsFooter";
 import HmsHeader from "../../components/header/HmsHeader";
+import HomeCarousel from "../../components/home-carousel/HomeCarousel";
 import "./Admin.css";
 
 const Admin = () => {
   return (
     <>
       <HmsHeader />
+      <div>
+        <img id="raccoon" src="/resources/raccoon.jpeg" alt="Raccoon" />
+      </div>
       <div className="content-wrapper-admin">
-        <div>
-          <img id="raccoon" src="/resources/raccoon.jpeg" alt="Raccoon" />
+        <div className="content-aside">
+          <center>
+            <h1 className="content-h1">Rooms</h1>
+          </center>
+          <p id="p-content">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit ipsum
+            quo consectetur nulla unde, ea amet cumque voluptatem, nam quam
+            libero alias impedit similique, autem sapiente delectus nisi
+            quisquam doloremque? Nihil possimus temporibus veniam sequi, aliquam
+            quia error soluta eius libero suscipit iure veritatis tenetur sit
+            adipisci, sapiente repellendus dolore, reiciendis fugit. Mollitia,
+            vitae sit. Omnis, ipsa. Molestiae quasi ea doloremque voluptatum
+            aliquid aliquam labore facere, consectetur natus animi error nobis
+            hic, quas quis, tenetur voluptatem reprehenderit optio ducimus.
+            Corporis inventore dicta nihil perferendis at ex. Maxime molestiae
+            numquam temporibus labore vel quam veritatis iste repudiandae eius
+            illum ad dignissimos, modi quo rerum aliquid nulla consequuntur
+            laboriosam quaerat facere facilis. Incidunt commodi numquam, eveniet
+            corrupti accusamus praesentium officiis dolore, magni nostrum ipsum
+            dolorum libero delectus amet sapiente repellat ipsam ea. Voluptates
+            eaque eius ipsa inventore magni repudiandae, temporibus, quaerat at
+            voluptatum illum porro alias cupiditate quisquam excepturi cum
+            labore quae.
+          </p>
+          <HomeCarousel />
         </div>
-        <div>
-          <p id = "p-content">
+        <div className="content-aside">
+          <p id="p-content">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit ipsum
             quo consectetur nulla unde, ea amet cumque voluptatem, nam quam
             libero alias impedit similique, autem sapiente delectus nisi
@@ -57,81 +84,6 @@ const Admin = () => {
             harum consectetur similique odio est officia consequuntur! Obcaecati
             laudantium blanditiis commodi iusto repudiandae saepe cum
             consectetur ipsa, placeat laborum possimus eveniet ad, quis tempore
-            quo fuga. Non nemo quo facere suscipit alias harum magni quae
-            inventore! Soluta, excepturi. Facilis placeat quaerat cum dolor?
-            Optio animi aut nam quaerat, id, aliquid labore eius atque dolore
-            illo iure. Ipsam molestias doloribus adipisci accusantium
-            voluptatibus debitis dolorum quis, assumenda repellendus beatae ea
-            ullam odit repellat! Consequuntur pariatur, fugiat obcaecati quae
-            magnam ab ipsam amet, reprehenderit officiis magni eveniet ipsa
-            suscipit nostrum! Ut enim veritatis magni, blanditiis tempora
-            dolorem non quibusdam porro commodi natus, quos, voluptas eligendi
-            laudantium alias veniam illo delectus minus fugit deserunt totam
-            ipsum vero molestias? Quam facilis officia, quae aspernatur tempora
-            fugit. Eum quibusdam, tempore quisquam repellendus atque fuga
-            corrupti sunt reiciendis accusantium porro doloribus dolorum optio?
-            Illo, expedita? Aspernatur vel, ducimus voluptas corrupti tempora
-            sequi. Cupiditate quos nostrum ad ducimus? Pariatur quas nesciunt in
-            harum. Earum id aliquam obcaecati reprehenderit repellendus
-            voluptatem, velit ea fuga exercitationem iusto et aperiam ratione
-            temporibus porro quasi, numquam quo natus maiores. Itaque pariatur
-            architecto quae maxime, minus, aliquam error reiciendis aut aperiam
-            accusamus, inventore accusantium repellendus doloremque porro
-            delectus voluptatum. Maxime velit ipsum sunt, eius dolorum unde
-            pariatur veritatis voluptatem possimus. Sit eligendi nam, officia
-            dolorum soluta ipsa voluptatibus ipsum exercitationem quidem unde
-            iure nulla deleniti quibusdam saepe. Veritatis, pariatur consequatur
-            eligendi autem fuga deleniti harum iusto quo nesciunt laborum
-            reprehenderit magni veniam atque consequuntur ex officiis doloribus
-            inventore illum, dolor eum repellat? Sed ratione doloribus fugit,
-            dolorum, nulla saepe modi, sit dolorem minus magni necessitatibus
-            assumenda aliquam distinctio soluta aliquid dolore. Voluptatibus
-            dolorem enim, nostrum, iste quod similique consequatur tempore
-            recusandae sequi vel fugiat vero beatae facere et fuga nisi culpa
-            esse reiciendis molestias a deleniti inventore. Saepe vitae
-            quibusdam earum possimus exercitationem deserunt sequi sunt.
-            Recusandae, ex eos voluptatem maxime sint voluptates quis delectus
-            expedita quia labore repellendus laborum, tempore, placeat
-            veritatis. Vel aliquam, laudantium facilis vitae ullam, quos
-            obcaecati facere quod ea perferendis ab quam cumque? Fugit
-            consequatur animi perspiciatis, ad cupiditate fugiat quaerat
-            molestias iste aliquid minus, quasi eos nostrum iure beatae, aut
-            accusamus. Recusandae deserunt sit ea saepe velit illum, enim porro
-            officia a quas consequatur corporis tenetur tempora rem rerum atque
-            debitis beatae doloremque, laudantium exercitationem! Dolorum
-            corporis explicabo voluptatum vel, voluptates dolorem ratione
-            reiciendis quo numquam laboriosam, distinctio cumque optio. Maiores,
-            qui veniam sed tempora, totam cumque exercitationem est expedita in
-            amet optio minus officiis saepe velit accusamus ullam, eius
-            consectetur voluptate distinctio. Eaque repellendus, explicabo
-            asperiores earum enim impedit vitae tempore. Laborum omnis,
-            cupiditate beatae pariatur, quos consectetur ea dolores iure ipsam
-            numquam natus? Perspiciatis commodi unde est incidunt quae maxime
-            eligendi inventore odit earum nulla voluptas exercitationem
-            perferendis hic quo voluptate fugit eveniet libero impedit quaerat
-            non officiis, nisi voluptatum. Animi sed quam rerum exercitationem,
-            totam eveniet aut aspernatur ut dolores. Quidem magni obcaecati quo
-            eius natus suscipit laboriosam minima quos pariatur illum? Deleniti
-            provident, nihil quae possimus et at illo necessitatibus neque
-            itaque incidunt sapiente temporibus molestias sed accusamus ducimus
-            harum. Eligendi neque enim ad cupiditate asperiores commodi illo
-            deleniti dolore velit inventore molestias error obcaecati, rerum
-            laudantium. Deserunt modi consequatur, perferendis porro magnam,
-            culpa inventore dolores maiores tempora earum nisi officiis soluta
-            quidem cupiditate minima commodi quas assumenda autem blanditiis cum
-            reprehenderit tempore ex alias sequi. Neque nam eveniet id iusto,
-            perspiciatis, iste libero est animi dolorem dolore velit aut
-            laboriosam placeat labore minima mollitia expedita. Soluta ea
-            veniam, eum voluptatibus commodi et! Nisi vitae eum aut rerum alias
-            doloremque voluptatibus quam id. Voluptatem vitae quisquam ratione
-            magni distinctio possimus officiis labore odit! Illum repellendus
-            quasi nostrum dignissimos enim deserunt odit, atque at quis,
-            possimus quisquam sint iste asperiores omnis ullam mollitia libero
-            sed quibusdam amet temporibus nihil! Voluptates sint cumque ut
-            dolore, commodi natus doloribus aliquid explicabo, iure odio earum
-            ratione! Libero, repudiandae exercitationem cum possimus dolorum,
-            architecto fugit eos facilis perspiciatis numquam molestiae.
-            Dolorum, iure!
           </p>
         </div>
       </div>
